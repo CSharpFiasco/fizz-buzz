25c6ed6f459cc07bb6695bfb5ee7d72c4d8a7e84
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2025-05-23 16:22:34 -0500

    WIP: submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatInputModule, ReactiveFormsModule, MatToolbarModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'fizz-buzz';
+  protected readonly title = 'fizz-buzz';
+
+  protected readonly formGroup = new FormGroup({
+    fizz: new FormControl<number>(3),
+    buzz: new FormControl<number>(5),
+    maxNumber: new FormControl<number>(100),
+    fizzWordToPrint: new FormControl<string>('Fizz'),
+    buzzWordToPrint: new FormControl<string>('Buzz'),
+  })
+
+  protected onSubmit() {
+
+  }
+
 }
